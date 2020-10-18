c93981ecf6141f67b1ed515dfcc0d26cf41cb04a
Garrett Brown <garrettpaulbrown@gmail.com>
2020-10-18 12:18:31 -0700

    Add routes to app.rb.

--- app.rb
+++ app.rb
@@ -3,9 +3,37 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 
 get('/') do
-  redirect to ('/home')
+  "This is home page."
 end
 
-get('/home') do
-  erb(:home)
+get('/words') do
+  "This route will show a list of all words."
+end
+
+get('/words/new') do
+  "This route will take us to a page with a form for adding new words."
+end
+
+get('/words/:id') do
+  "Route will show specific word based on ID. Value of ID here is #{params[:id]}"
+end
+
+post ('/words') do
+  "This route will add a word to our list of words."
+end
+
+get('/words/:id/edit') do
+  "This will take us to a page with a form for updating words with an ID of #{params[:id]}"
+end
+
+patch('/words/:id') do
+  "This route will update a word."
+end
+
+delete('/words/:id') do
+  "This route will delete a word."
+end
+
+get('/custom_route') do
+  "Create custom routes."
 end
