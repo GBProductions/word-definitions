e018691e4662569f524c39ffd1f8403002a62431
Garrett Brown <garrettpaulbrown@gmail.com>
2020-10-18 18:15:43 -0700

    Pass Definition tests.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -14,19 +14,19 @@ end
 
 describe('#==') do
 it("is the same definition if it has the same attributes as another definition") do
-  definition = Definition.new("Dogs", @word.id nil)
-  definition2 = Definition.new("Dogs", @word.id nil)
+  definition = Definition.new("Dogs", @word.id, nil)
+  definition2 = Definition.new("Dogs", @word.id, nil)
   expect(definition).to(eq(definition2))
   end
 end
 
   describe(".all") do
     it("returns a list of all definitions") do
-      definition = Definition.new("Dogs", @word.id nil)
+      definition = Definition.new("Dogs", @word.id, nil)
       definition.save()
-      definition2 = Definition.new("Dogs", @word.id nil)
+      definition2 = Definition.new("Cats", @word.id, nil)
       definition2.save()
-      expect(Definition.all).to(eq(definition, definition2))
+      expect(Definition.all).to(eq([definition, definition2]))
     end
   end
 
@@ -34,7 +34,7 @@ end
     it("clear all definitions") do
       definition = Definition.new("Dogs", @word.id, nil)
       definition.save()
-      definition2 = Definition.new("Cats", @word.id nil)
+      definition2 = Definition.new("Cats", @word.id, nil)
       definition2.save()
       Word.clear()
       expect(Word.all).to(eq([]))
@@ -45,7 +45,7 @@ end
     it("saves a definition") do
       definition = Definition.new("Dogs", @word.id, nil)
       definition.save()
-      expect(Definition.all).to(eq([]))
+      expect(Definition.all).to(eq([definition]))
     end
   end
 
@@ -53,9 +53,9 @@ end
     it("finds a definition by id") do
       definition = Definition.new("Dogs", @word.id, nil)
       definition.save()
-      definition2 = Definition.new("Cats", @word.id nil)
+      definition2 = Definition.new("Cats", @word.id, nil)
       definition2.save()
-      expect(Definition.find(definition.id)).to(eq(song))
+      expect(Definition.find(definition.id)).to(eq(definition))
     end
   end
 
